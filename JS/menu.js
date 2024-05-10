e65f8f102bc5403fe1850b2ad2f4d29ab5e7a5ae
Vernon30 <146816263+Vernon30@users.noreply.github.com>
2024-05-10 12:20:14 +0200

    Add files via upload

---
+++ JS/menu.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+    const navbarLinks = document.querySelector(".navbar-links");
+    navbarLinks.classList.toggle("open"); // Toggle the 'open' class to show/hide the navbar links
+  }
+
+function openLinkin() {
+  const url = "https://www.example.com"; // The URL to navigate to
+  window.location.href = url; // Redirects to the URL in the same tab
+  }
+
+function openGitHub() {
+  const url = "https://www.example.com"; // The URL to navigate to
+  window.location.href = url; // Redirects to the URL in the same tab
+  }
